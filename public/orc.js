52c65280ab0976845939f56d2ab24e7523472690
Yurasik228 <yurii.garpul@gmail.com>
2019-10-27 16:01:24 +0200

    Lesson_5

---
+++ public/orc.js
@@ -0,0 +1,34 @@
+function playS( id ) {
+    const audio = document.getElementById('snd'+id);
+    const divkey= document.getElementById('k'+id);
+    audio.currentTime = 0;
+    audio.play();
+    divkey.className = 'acrive';
+
+}
+function clearS( id ){
+    const divkey = document.getElementById('k'+id)
+    divkey.className = '';
+}
+ 
+
+
+ 
+
+function myKeysDw( e ){
+    switch (e.keyCode) {
+        case 65: playS ('01'); break;
+        case 83: playS ('02'); break;
+        case 68: playS ('03'); break;
+        default: console.log(e);
+            break;
+    }
+}
+function myKeysUp( e ){
+    switch (e.keyCode) {
+        case 65: clearS ('01'); break;
+        case 83: clearS ('02'); break;
+        case 68: clearS ('03'); break;
+        default: console.log(e);
+            break;
+}
