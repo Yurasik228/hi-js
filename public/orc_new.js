c64667bc117307f687dae2abd0016fd9dc44c10e
Yurasik228 <yurii.garpul@gmail.com>
2019-11-10 16:01:18 +0200

    Lesson_6

---
+++ public/orc_new.js
@@ -0,0 +1,25 @@
+function playS ( kc ){
+    const audio = document.querySelector('audio[data-key="'+ kc +'"]');
+    const divkey = document.querySelector('div[data-key="'+ kc +'"]')
+    if (!audio) {return;}
+    if (!divkey) {return;}
+    audio.currentTime = 0;
+    audio.play();
+    divkey.className= 'active';
+}
+
+function clearS ( kc ){
+    const divkey = document.querySelector(' div[data-key="'+ kc +'"]' );
+    if (!divkey) {return;}
+    divkey.className = '';
+}
+function myKeysDown ( e ){
+    playS(e.keyCode);
+} 
+function myKeysUp( e ){
+    clearS( e.keyCode )
+}
+
+
+   
+    
