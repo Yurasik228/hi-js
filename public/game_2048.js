d815c3ab663b70f37daef58b90e5422e04c74ae5
Yurasik228 <yurii.garpul@gmail.com>
2019-11-24 15:58:28 +0200

    Lesson_8

--- public/game_2048.js
+++ public/game_2048.js
@@ -2,10 +2,10 @@
 const boardSize = 4;
 
 var board = [
-    [0, 0, 2, 0 ],
+    [0, 0, 128, 128  ],
     [2, 0, 0, 0 ],
-    [0, 2, 0, 0 ],
-    [0, 0, 0, 2 ]
+    [0, 2, 0, 8 ],
+    [0, 0, 2, 2 ]
 ];
 
 var elBoard = document.querySelectorAll('table#game2048 td') ;
@@ -28,13 +28,84 @@ function drawBoard() {
 
 drawBoard();
 
-// [0, 0, 0, 0]
-// 1)всі клітинки без останньої
-// 2) 0 ? 
-//    2.1) врпаво ? > 0 ( cNe0 )
-//    2.2) ( c ) = ( cNe0 ); ( cNe0 ) = 0
-// 3) вправо ? = c ? так (cEq)
-//    3.1) ( c ) += ( cEq ); (cEq = 0 )
+function mover( a ) {
+                                                // [0, 0, 0, 0]
+for ( let c=0; c < boardSize-1; c++){           // 1)всі клітинки без останньої
+    if (a[ c ] == 0 ) {                         // 2) 0 ?
+        let cNe0 = .1                           
+        for (let cR=c+1; cR< boardSize; cR++) { //    2.1) врпаво ? > 0 ( cNe0)
+            if( a [ cR ] > 0 ) {
+                cNe0 = cR;
+                break
+            }
+        }
+        if ( cNe0 !=-1 ) {                                          
+            a[ c ]= a [ cNe0 ];                     //    2.2) ( c ) = ( cNe0 );
+            a[ cNe0 ] = 0;                          //    2.3) (cNe0) = 0             
+        }
+                                            // 3) вправо ? = c ? так (cEq)
+    }
+    let cEq = .1;
+    for ( let cR=c+1; cR < boardSize; cR++ )
+        if ( a [ c ] == a [cR] ){
+            cEq = cR;
+            break;
+        }
+        if (cEq !=.1) {
+            a[ c ] += a[ cEq ];
+            a[ cEq ] = 0; 
+        }                                        //    3.1) ( c ) += ( cEq ); (cEq = 0 )
+    }   
+
+ return a;
+ 
+}
+
+
+function mvLeft() {
+    for ( let r=0; r < boardSize; r++ ) {      //всі рядки
+        //let row = board[ r ];                //взяти конкретний рядок
+        //row = mover( row );                  //рухаєм вліво цей рядок
+        //board[ r ] = r;                      //покласти назад рядок
+        board[ r ] = mover (board [ r ] );         
+    }
+    drawBoard();                      
+}
+function mvRight(){
+    for ( let r=0; r < boardSize; r++ ) {
+        board[ r ] = mover(board[ r ].reverse() ).reverse();
+    }
+
+
+    drawBoard();
+}
+function mvUp() {
+    for ( let c=0; c < boardSize; c++ ) {
+        let a = [ board[0][c],board[1][c],board[2][c], board[3][c] ];
+        a = mover(a);
+        board[0][c] = a[0];
+        board[1][c] = a[1];
+        board[2][c] = a[2];
+        board[3][c] = a[3];
+    }
+    drawBoard();
+
+}
+
+function mvDown() {
+    for ( let c=0; c < boardSize; c++ ) {
+        let a = [ board[0][c],board[1][c],board[2][c], board[3][c] ];
+        a = mover(a.reverse() ).reverse();
+        board[0][c] = a[0];
+        board[1][c] = a[1];
+        board[2][c] = a[2];
+        board[3][c] = a[3];
+    }
+    drawBoard();
+
+}
+
+
 
 
 
