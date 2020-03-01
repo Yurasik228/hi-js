bce944b1175b84511e6e503c2178f8e13758a48b
Yurasik228 <yurii.garpul@gmail.com>
2020-03-01 15:54:39 +0200

    boxis

--- public/move-the-box-handler.js
+++ public/move-the-box-handler.js
@@ -22,18 +22,83 @@ function mouseOver( e ){
 
 
 function moveUp() {
+    if (selBox.length > 0 && selBox[0] > 0) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if (board[r-1][c] == 0) {
+            board[r-1][c] = board[r][c];
+            board[r][c] = 0;
+            moveBox([r,c], [r-1,c])
+        } else {
+            let t = board[r][c];
+            board[r][c] = board[r - 1][c];
+            board[r - 1][c] = t;
+            moveBox([r,c], [r-1,c])
+            moveBox([r-1,c], [r,c])
+        }
+    }
+        selBox = [];
     logs('UP')
+
 }
 
 function moveLeft() {
+    if (selBox.length > 0 && selBox[0] > 0) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if (board[r][c-1] == 0) {
+            board[r][c-1] = board[r][c];
+            board[r][c] = 0;
+            moveBox([r,c], [r,c-1])
+        } else {
+            let t = board[r][c];
+            board[r][c] = board[r][c-1];
+            board[r][c-1] = t;
+            moveBox([r,c], [r,c-1])
+            moveBox([r,c-1], [r,c])
+        }
+    }
+        selBox = [];
     logs('LEFT')
 }
 
 function moveRight() {
+    if (selBox.length > 0 && selBox[0] > 0) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if (board[r][c+1] == 0) {
+            board[r][c+1] = board[r][c];
+            board[r][c] = 0;
+            moveBox([r,c], [r,c+1])
+        } else {
+            let t = board[r][c];
+            board[r][c] = board[r][c+1];
+            board[r][c+1] = t;
+            moveBox([r,c], [r,c+1])
+            moveBox([r,c+1], [r,c])
+        }
+    }
+        selBox = [];
     logs('RIGHT')
 }
 
 function moveDown() {
+    if (selBox.length > 0 && selBox[0] > 0) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if (board[r+1][c] == 0) {
+            board[r+1][c] = board[r][c];
+            board[r][c] = 0;
+            moveBox([r,c], [r+1,c])
+        } else {
+            let t = board[r][c];
+            board[r][c] = board[r + 1][c];
+            board[r + 1][c] = t;
+            moveBox([r,c], [r+1,c])
+            moveBox([r+1,c], [r,c])
+        }
+    }
+        selBox = [];
     logs('DOWN')
 }
 
